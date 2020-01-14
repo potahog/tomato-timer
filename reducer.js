a579f157cb24c2f9ffcde1d4193229f6f9a939a0
오명훈 <potahog@omyeonghun-ui-MacBookPro.local>
2020-01-14 13:38:03 +0900

    Creating the tomato reducer

---
+++ reducer.js
@@ -0,0 +1,91 @@
+// Import
+
+// Actions
+
+const START_TIMER = "START_TIMER";
+const RESTART_TIMER = "RESTART_TIMER";
+const ADD_SECONDS = "ADD_SECOND";
+
+// Actions Creator
+
+function startTimer() {
+  return {
+    type: START_TIMER
+  };
+}
+
+function restartTImer() {
+  return {
+    type: RESTART_TIMER
+  };
+}
+
+function addSecond() {
+  return {
+    type: ADD_SECONDS
+  };
+}
+
+// Reducer
+
+const TIMER_DURATION = 1500;
+
+const initialState = {
+  isPlaying: false,
+  elapsedTime: 0,
+  timerDuraction: TIMER_DURATION
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case START_TIMER:
+      return applyStartTimer(state);
+    case RESTART_TIMER:
+      return applyRestartTimer(state);
+    case ADD_SECONDS:
+      return applyAddSeconds(state);
+  }
+}
+
+// Reducer Functions
+
+function applyStartTimer(state) {
+  return {
+    ...state,
+    isPlaying: true
+  };
+}
+
+function applyRestartTimer(state) {
+  return {
+    ...state,
+    isPlaying: false,
+    elapsedTime: 0
+  };
+}
+
+function applyAddSeconds(state) {
+  if (state.elapsedTime < TIMER_DURATION) {
+    return {
+      ...state,
+      elapsedTime: state.elapsedTime + 1
+    };
+  } else {
+    return {
+      ...state,
+      isPlaying: false
+    };
+  }
+}
+
+// Export Action Creators
+
+const actionCreators = {
+  startTimer,
+  restartTImer,
+  addSecond
+};
+
+// Export Reducer
+
+export default reducer;
